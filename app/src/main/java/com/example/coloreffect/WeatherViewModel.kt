fe9e4611e6e0b41280d729f5c7f13f256988f116
CasperKHV <kirill1092@yandex.ru>
2024-03-07 16:20:18 +0300

    New way for joins ViewModel to MainActivity

--- app/src/main/java/com/example/coloreffect/WeatherViewModel.kt
+++ app/src/main/java/com/example/coloreffect/WeatherViewModel.kt
@@ -3,12 +3,13 @@ package com.example.coloreffect
 import android.util.Log
 import androidx.lifecycle.ViewModel
 
-class WeatherViewModel: ViewModel(){
+class WeatherViewModel : ViewModel() {
+
     init {
-        Log.d("WeatherViewModel","WeatherViewModel is created")
+        Log.d("WeatherViewModel", "WeatherViewModel is created")
     }
 
     override fun onCleared() {
-        Log.d("WeatherViewModel","onCleared")
+        Log.d("WeatherViewModel", "onCleared")
     }
 }
