3ada8da0e9bb3f996bd62c5405a2a292d76a512b
CasperKHV <kirill1092@yandex.ru>
2024-03-20 15:59:51 +0300

    delete log from another activity

--- app/src/main/java/com/casperkhv/weather/WeatherResult.kt
+++ app/src/main/java/com/casperkhv/weather/WeatherResult.kt
@@ -4,10 +4,9 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
-class WeatherResult : AppCompatActivity() {
+internal class WeatherResult : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(TAG, "onCreate savedInstanceState$savedInstanceState")
         setContentView(R.layout.activity_weather_result)
         val resultFragment: WeatherResultFragment = WeatherResultFragment.Companion.newInstance(
             intent.getSerializableExtra(WeatherResultFragment.Companion.DATA_FOR_BUNDLE)
@@ -15,31 +14,6 @@ class WeatherResult : AppCompatActivity() {
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.fragment_container, resultFragment)
         transaction.commit()
-    }
-
-    override fun onStart() {
-        Log.d(TAG, "onStart")
-        super.onStart()
-    }
-
-    override fun onResume() {
-        Log.d(TAG, "onResume")
-        super.onResume()
-    }
-
-    override fun onPause() {
-        Log.d(TAG, "onPause")
-        super.onPause()
-    }
-
-    override fun onStop() {
-        Log.d(TAG, "onStop")
-        super.onStop()
-    }
-
-    override fun onDestroy() {
-        Log.d(TAG, "onDestroy")
-        super.onDestroy()
     }
 
     companion object {
