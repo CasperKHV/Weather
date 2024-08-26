6745de8b7a562d0f6564ac1e4a5b5c06c3aff626
Andrei Erdman <andrei.erdman.27@gmail.com>
2024-08-26 21:01:17 +0300

    add ui

--- temperature_feature/src/main/java/com/casperkhv/weather/temperature/presentation/TemperatureFragment.kt
+++ temperature_feature/src/main/java/com/casperkhv/weather/temperature/presentation/TemperatureFragment.kt
@@ -4,21 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.casperkhv.weather.base_device.view.bindView
 import com.casperkhv.weather.temperature.R
 
 internal class TemperatureFragment : Fragment() {
 
     private val viewModel: TemperatureViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private val temperatureTextView by bindView<TextView>(R.id.temperature_text_view)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
         return inflater.inflate(R.layout.fragment_temperature, container, false)
     }
